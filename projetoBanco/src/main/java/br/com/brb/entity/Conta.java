2f8aa86707ad84971dab24992dfb1a1df5bdae6e
gabriel.vogado <gabriel.vogado@INV005151.dynatec.com.br>
2017-09-19 10:43:17 -0300

    Usuario consegue logar mas não sai da pagina de Login.

--- projetoBanco/src/main/java/br/com/brb/entity/Conta.java
+++ projetoBanco/src/main/java/br/com/brb/entity/Conta.java
@@ -18,7 +18,7 @@ public class Conta implements Serializable {
 	private static final long serialVersionUID = 1L;
 
 	@Id
-	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	@GeneratedValue(strategy=GenerationType.AUTO)
 	@Column(name="idconta")
 	private int id;
 	
